811c4fca3a3fe4d453d3e91172af813fbecf0024
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-11-01 13:57:03 +0900

    課題完成

--- shoryukenApp/ViewController.swift
+++ shoryukenApp/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var imageNameArray : Array<UIImage> = []
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        while let attakImage = UIImage(named: "attak\(imageNameArray.count+1)") {
+            imageNameArray.append(attakImage)
     }
+}
 
-
+    @IBAction func tapImageButton(_ sender: Any) {
+        imageView.animationImages = imageNameArray
+        imageView.animationDuration = 2.5
+        imageView.animationRepeatCount = 1
+        imageView.startAnimating()
+    }
+    
 }
 
